1fe6ede6738d61b8847b63d6edb1380a08e82b62
Karishma Ghiya <kaghiya@microsoft.com>
2019-06-10 14:07:57 -0700

    fixed the build to succeed

--- lib-bee/index.ts
+++ lib-bee/index.ts
@@ -1,7 +1,7 @@
 /// <reference lib="es2015" />
 /// <reference lib="esnext.asynciterable" />
-var Buffer = require('buffer/').Buffer 
-const buf = Buffer.from('hello world', 'ascii');
+const buff = require('buffer/').Buffer 
+const bufferConversionResult = buff.from('hello world', 'ascii');
 console.log("lib-bee printing base64 for buffer - 'hello world', 'ascii'");
-console.log(buf.toString('base64'));
+console.log(bufferConversionResult.toString('base64'));
 // should print: aGVsbG8gd29ybGQ=
