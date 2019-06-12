a28ea109fa15d6fdb3b99a3765f3316aba1bb304
Karishma Ghiya <kaghiya@microsoft.com>
2019-06-12 10:55:41 -0700

    move over to terser from uglify

--- lib-aaa/rollup.base.config.js
+++ lib-aaa/rollup.base.config.js
@@ -2,7 +2,7 @@ import nodeResolve from "rollup-plugin-node-resolve";
 import multiEntry from "rollup-plugin-multi-entry";
 import cjs from "rollup-plugin-commonjs";
 import replace from "rollup-plugin-replace";
-import { uglify } from "rollup-plugin-uglify";
+import { terser } from "rollup-plugin-terser";
 import sourcemaps from "rollup-plugin-sourcemaps";
 import viz from "rollup-plugin-visualizer";
 
@@ -44,7 +44,7 @@ export function nodeConfig(test = false) {
     // mark assert as external
     baseConfig.external.push("assert");
   } else if (production) {
-    baseConfig.plugins.push(uglify());
+    baseConfig.plugins.push(terser());
   }
 
   return baseConfig;
@@ -90,7 +90,7 @@ export function browserConfig(test = false, production = false) {
     baseConfig.output.file = "test-browser/index.js";
   } else if (production) {
     baseConfig.output.file = "browser/lib-aaa.min.js";
-    baseConfig.plugins.push(uglify());
+    baseConfig.plugins.push(terser());
   }
 
   return baseConfig;
